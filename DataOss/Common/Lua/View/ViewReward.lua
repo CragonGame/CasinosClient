73842ebc3540ad3c2a0723d2499f7f4d3857a431
li.yong <lion@cragon.cn>
2018-11-27 01:57:37 +0800

    福利模块调整。

--- DataOss/Common/Lua/View/ViewReward.lua
+++ DataOss/Common/Lua/View/ViewReward.lua
@@ -1,6 +1,89 @@
 -- Copyright(c) Cragon. All rights reserved.
 
 ---------------------------------------
+-- 在线奖励
+UiRewardOnline = {
+    CasinosContext = CS.Casinos.CasinosContext.Instance;
+    LuaMgr = CS.Casinos.CasinosContext.Instance.LuaMgr;
+    ViewMgr = nil;
+    ComUi = nil;
+    GTextOnlineCountDownTm = 0;
+    GBtnOnlineReward = nil
+}
+
+function UiRewardOnline:Create(view_mgr, com_ui)
+    self.ViewMgr = view_mgr
+    self.ComUi = com_ui
+    local com_rewardonline = self.ComUi:GetChild("RewardOnline").asCom
+
+    self.GTextOnlineCountDownTm = com_rewardonline:GetChild("TextCountdown").asTextField
+    self.GBtnOnlineReward = com_rewardonline:GetChild("BtnGetOnlineReward").asButton
+    self.GBtnOnlineReward.onClick:Add(
+            function()
+                self:_onClickBtnOnlineReward()
+            end
+    )
+end
+
+function UiRewardOnline:Destroy()
+    self.GBtnOnlineReward.onClick:Clear()
+end
+
+function UiRewardOnline:SetOnlineRewardLeftTm(left_tm)
+    self.GTextOnlineCountDownTm.text = left_tm
+end
+
+function UiRewardOnline:_onClickBtnOnlineReward()
+    local ev = self.ViewMgr:GetEv("EvViewOnGetOnLineReward")
+    if (ev == nil) then
+        ev = EvOnGetOnLineReward:new(nil)
+    end
+    self.ViewMgr:SendEv(ev)
+end
+
+---------------------------------------
+-- 定时奖励
+UiRewardTiming = {
+    CasinosContext = CS.Casinos.CasinosContext.Instance;
+    LuaMgr = CS.Casinos.CasinosContext.Instance.LuaMgr;
+    ViewMgr = nil;
+    ComUi = nil;
+    GBtnTimingReward = nil;
+    CanGetTimingReward = false
+}
+
+function UiRewardTiming:Create(view_mgr, com_ui)
+    self.ViewMgr = view_mgr
+    self.ComUi = com_ui
+    local com_rewardtiming = self.ComUi:GetChild("RewardTiming").asCom
+
+    self.GBtnTimingReward = com_rewardtiming:GetChild("BtnGetTimingReward").asButton
+    self.GBtnTimingReward.onClick:Add(
+            function()
+                self:_onClickBtnTimingReward()
+            end
+    )
+end
+
+function UiRewardTiming:Destroy()
+    self.GBtnTimingReward.onClick:Clear()
+end
+
+function UiRewardTiming:SetCanGetTimingReward(can_get_reward)
+    self.CanGetTimingReward = can_get_reward
+end
+
+function UiRewardTiming:_onClickBtnTimingReward()
+    if self.CanGetTimingReward then
+        local ev = self.ViewMgr:GetEv("EvViewRequestGetTimingReward")
+        if (ev == nil) then
+            ev = EvRequestGetTimingReward:new(nil)
+        end
+        self.ViewMgr:SendEv(ev)
+    end
+end
+
+---------------------------------------
 ViewReward = ViewBase:new()
 
 ---------------------------------------
@@ -17,6 +100,8 @@ function ViewReward:new(o)
     o.ViewKey = nil
     o.Tween = nil
     o.CanGetTimingReward = false
+    o.UiRewardOnline = UiRewardOnline
+    o.UiRewardTiming = UiRewardTiming
     return o
 end
 
@@ -38,24 +123,8 @@ function ViewReward:OnCreate()
             end
     )
 
-    local com_rewardonline = self.ComUi:GetChild("RewardOnline").asCom
-
-    self.GTextOnlineCountDownTm = com_rewardonline:GetChild("TextCountdown").asTextField
-    self.GBtnOnlineReward = com_rewardonline:GetChild("BtnGetOnlineReward").asButton
-    self.GBtnOnlineReward.onClick:Add(
-            function()
-                self:_onClickOnlineReward()
-            end
-    )
-
-    local com_rewardtiming = self.ComUi:GetChild("RewardTiming").asCom
-
-    self.GBtnTimingReward = com_rewardtiming:GetChild("BtnGetTimingReward").asButton
-    self.GBtnTimingReward.onClick:Add(
-            function()
-                self:_onClickTimingReward()
-            end
-    )
+    self.UiRewardOnline:Create(self.ViewMgr, self.ComUi)
+    self.UiRewardTiming:Create(self.ViewMgr, self.ComUi)
 end
 
 ---------------------------------------
@@ -64,41 +133,24 @@ function ViewReward:OnDestroy()
         self.Tween:Kill(false)
         self.Tween = nil
     end
+
+    self.UiRewardOnline:Destroy()
+    self.UiRewardTiming:Destroy()
 end
 
 ---------------------------------------
 function ViewReward:SetOnlineRewardLeftTm(left_tm)
-    self.GTextOnlineCountDownTm.text = left_tm
+    self.UiRewardOnline:SetOnlineRewardLeftTm(left_tm)
 end
 
 ---------------------------------------
 function ViewReward:SetCanGetTimingReward(can_get_reward)
-    self.CanGetTimingReward = can_get_reward
+    self.UiRewardTiming:SetCanGetTimingReward(can_get_reward)
 end
 
 ---------------------------------------
 function ViewReward:_onClickBtnClose()
     self.ViewMgr:DestroyView(self)
-end
-
----------------------------------------
-function ViewReward:_onClickOnlineReward()
-    local ev = self.ViewMgr:GetEv("EvViewOnGetOnLineReward")
-    if (ev == nil) then
-        ev = EvOnGetOnLineReward:new(nil)
-    end
-    self.ViewMgr:SendEv(ev)
-end
-
----------------------------------------
-function ViewReward:_onClickTimingReward()
-    if self.CanGetTimingReward then
-        local ev = self.ViewMgr:GetEv("EvViewRequestGetTimingReward")
-        if (ev == nil) then
-            ev = EvRequestGetTimingReward:new(nil)
-        end
-        self.ViewMgr:SendEv(ev)
-    end
 end
 
 ---------------------------------------
