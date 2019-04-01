805153ecc93c49c7a0e931984ec4e15bf3c6b3bf
arong2100 <190324618@qq.com>
2019-04-01 10:37:10 +0800

    1.openInstall

--- Assets/Script/Main/MbLaunch.cs
+++ Assets/Script/Main/MbLaunch.cs
@@ -232,6 +232,7 @@ namespace Casinos
             AppDomain.DelegateManager.RegisterMethodDelegate<FairyGUI.GTweener>();
             AppDomain.DelegateManager.RegisterMethodDelegate<int, FairyGUI.GObject>();
             AppDomain.DelegateManager.RegisterMethodDelegate<Int32, cn.sharesdk.unity3d.ResponseState, cn.sharesdk.unity3d.PlatformType, Hashtable>();
+            AppDomain.DelegateManager.RegisterMethodDelegate<io.openinstall.unity.OpenInstallData>();
 
             AppDomain.DelegateManager.RegisterDelegateConvertor<cn.sharesdk.unity3d.ShareSDK.EventHandler>((act) => 
             {
